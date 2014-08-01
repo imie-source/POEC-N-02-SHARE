4a20658eb076afeae76f88bf492bf3a15930ae18
orlan <martin.orlan@wanadoo.fr>
2014-08-01 13:30:37 +0200

    methode score  sans param

--- cedOrlan/src/fr/imie/test/Jeu.java
+++ cedOrlan/src/fr/imie/test/Jeu.java
@@ -2,9 +2,9 @@ package fr.imie.test;
 
 public class Jeu {
 	
-	public String score(String score){
+	public String score(){
 		
-		String leScore = score;
+		String leScore = "0-0";
 		
 		return leScore;
 	}
