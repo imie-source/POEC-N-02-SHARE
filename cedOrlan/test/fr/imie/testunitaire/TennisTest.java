c1272d38d31bb43728f362309e2dde214623314f
cedric Prudent <prudent.ced@gmail.com>
2014-08-01 13:36:20 +0200

    3eme test2

--- cedOrlan/test/fr/imie/testunitaire/TennisTest.java
+++ cedOrlan/test/fr/imie/testunitaire/TennisTest.java
@@ -18,11 +18,15 @@ public class TennisTest {
 		Jeu jeu = new Jeu();
 		assertEquals("0-0", jeu.score());
 	}
+	@Test
+	
+	// j'ai rectifier aussi car scored prend un jeu en param ^^
+	
 	public void testJoueurMarquePoint(){
 		Jeu jeu = new Jeu();
 		Joueur joueur = new Joueur();
 		joueur.scored();
-		assertEquals("15-0", joueur.scored());
+		assertEquals("15-0", joueur.scored(jeu));
 
 		
 	}
