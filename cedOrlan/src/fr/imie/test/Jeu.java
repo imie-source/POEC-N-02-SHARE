880946d497fc7b83e905a8621b68a18e559d0a18
orlan <martin.orlan@wanadoo.fr>
2014-08-01 16:15:41 +0200

    recup

--- cedOrlan/src/fr/imie/test/Jeu.java
+++ cedOrlan/src/fr/imie/test/Jeu.java
@@ -15,28 +15,7 @@ public class Jeu {
 		return score;
 	}
 	
-	
-	/*
-	private String score;
-	
-	
 
-	public Jeu(String score) {
-		
-		if (score== null){
-			this.score="0-0";
-		}else{
-		this.score = score;
-	}
-
-	public String getScore() {
-		return score;
-	}
-
-	public void setScore(String score) {
-		this.score = score;
-	}
-*/
 
 
 }
--- cedOrlan/src/fr/imie/test/Joueur.java
+++ cedOrlan/src/fr/imie/test/Joueur.java
@@ -2,11 +2,8 @@ package fr.imie.test;
 
 public class Joueur {
 
-	public String scored(Jeu jeu){
-		String score = "15-0";
-		
-		return  score;
-		
-	}
+
+
+	
 	
 }
