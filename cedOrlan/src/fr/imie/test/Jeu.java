75c046b36270865995c81b6aec6f88fd7dbdfec6
orlan <martin.orlan@wanadoo.fr>
2014-08-01 14:42:02 +0200

    modifs 14:41

--- cedOrlan/src/fr/imie/test/Jeu.java
+++ cedOrlan/src/fr/imie/test/Jeu.java
@@ -1,14 +1,42 @@
 package fr.imie.test;
 
 public class Jeu {
+
 	
 	
-	public String score(){
+	public Jeu(){
 		
-		String leScore = "0-0";
-		
-		return leScore;
 	}
 	
+	
+	public String score() {
+		
+		String score = "0-0";
+		return score;
+	}
+	
+	
+	/*
+	private String score;
+	
+	
+
+	public Jeu(String score) {
+		
+		if (score== null){
+			this.score="0-0";
+		}else{
+		this.score = score;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+*/
+
 
 }
